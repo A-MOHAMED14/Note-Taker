1777f483d2cc302efcec185dd185583bb3ac2a9a
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-05 10:58:47 +0100

    Add GET route handlers for notes and wildcard endpoints

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+app.get("/notes", (req, res) => {
+  console.log(`${req.method} request to ${req.path} recieved`);
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  console.log(`${req.method} request to ${req.path} recieved`);
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
