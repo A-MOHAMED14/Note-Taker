7656a12de511facdbc43992d33ae6dde020dcd60
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-05 13:09:16 +0100

    Create a route handler which deletes a note using the notes uuid

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -33,22 +33,60 @@ notes.post("/", (req, res) => {
 
         jsonData.push(newNote);
 
-        fs.writeFile("./db/db.json", JSON.stringify(jsonData, null,  4), (err) => {
-          if (err) {
-            console.error(err);
-            res.json("Error in posting note");
-          } else {
-            const response = {
-              status: "success",
-              body: newNote,
-            };
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(jsonData, null, 4),
+          (err) => {
+            if (err) {
+              console.error(err);
+              res.json("Error in posting note");
+            } else {
+              const response = {
+                status: "Note added successfully",
+                body: newNote,
+              };
 
-            res.json(response);
+              res.json(response);
+            }
           }
-        });
+        );
       }
     });
   }
+});
+
+notes.delete("/:id", (req, res) => {
+  const uuid = req.params.id;
+
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const jsonData = JSON.parse(data);
+
+      const noteIndex = jsonData.findIndex(
+        (note) => note.uuid === "deca8468-e94f-41a3-8031-4fbbc2180c64"
+      );
+
+      console.log("Note to delete index position is:", noteIndex);
+
+      jsonData.splice(noteIndex, 1);
+
+      fs.writeFile("./db/db.json", JSON.stringify(jsonData, null, 4), (err) => {
+        if (err) {
+          console.error(err);
+          res.json("Error deleting note");
+        } else {
+          const response = {
+            status: "Note deleted successfully",
+            body: jsonData,
+          };
+
+          res.json(response);
+        }
+      });
+    }
+  });
 });
 
 module.exports = notes;
