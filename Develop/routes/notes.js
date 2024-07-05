c7101c9243c8a049cc8549d3e2fa278a67ad90c5
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-05 11:52:55 +0100

    Create post route for a new note

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs/promises");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/", (req, res) => {
-  fs.readFile("../db/db.json", "utf-8", (err, data) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
@@ -12,3 +13,42 @@ router.get("/", (req, res) => {
     }
   });
 });
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      uuid: uuidv4(),
+    };
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const jsonData = JSON.parse(data);
+
+        jsonData.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(jsonData), (err) => {
+          if (err) {
+            console.error(err);
+            res.json("Error in posting note");
+          } else {
+            const response = {
+              status: "success",
+              body: newNote,
+            };
+
+            res.json(response);
+          }
+        });
+      }
+    });
+  }
+});
+
+module.exports = router;
