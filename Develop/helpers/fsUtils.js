68dd6d5ab8dd63219a8b18e5b254ecbac988e737
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-05 15:24:50 +0100

    Create a utility function which is able to read and write to a file

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -3,4 +3,50 @@ const util = require("util");
 
 const readFromFile = util.promisify(fs.readFile);
 
-module.exports = { readFromFile };
+const writeToFile = function (filePath, content) {
+  fs.writeFile(filePath, JSON.stringify(content, null, 4), (err) => {
+    err ? console.error(err) : console.info(`Data written to ${filePath}`);
+  });
+};
+
+const readAndAppend = function (filePath, content) {
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(filePath, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, readAndAppend };
+
+// fs.readFile("./db/db.json", "utf-8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     const jsonData = JSON.parse(data);
+
+//     jsonData.push(newNote);
+
+//     fs.writeFile(
+//       "./db/db.json",
+//       JSON.stringify(jsonData, null, 4),
+//       (err) => {
+//         if (err) {
+//           console.error(err);
+//           res.json("Error in posting note");
+//         } else {
+//           const response = {
+//             status: "Note added successfully",
+//             body: newNote,
+//           };
+
+//           res.json(response);
+//         }
+//       }
+//     );
+//   }
+// });
