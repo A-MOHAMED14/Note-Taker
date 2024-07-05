ffd4e675b7ff71c3935a522baa3640cf9ae85ac9
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-05 10:54:38 +0100

    Add middleware for parsing data and middleware to server up static files

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
